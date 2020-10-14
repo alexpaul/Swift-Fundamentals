f8e20dfbbfd0e8058bb90ef2f7c502f3133d6794
Alex Paul <alexpaul@users.noreply.github.com>
2020-10-14 17:09:21 -0400

    Create Types-Variables-Logic-Operations-Exercise.swift

---
+++ Types-Variables-Logic-Operations-Exercise.swift
@@ -0,0 +1,281 @@
+// Types Variables Logic and Operations Lab
+
+// 1.
+// Which of the following variables/constants are declared correctly?
+
+//let nameOfPrincipal: Character = "Mrs. Watkins"
+//
+//var temperatureOutside: Int = 90.7
+//
+//var isSummer: String = false
+//
+//let whiteHouseAddress: Int + String = 1600 + "Pennsylvania Ave"
+//
+//var peopleAtParty: Double = "95"
+
+
+
+// 2.
+// Which of the following expressions evaluate to true?
+
+//!(4 + 3 < 2 * 4)
+//
+//!(1 + 1 != 2) && !(3 >= 3)
+//
+//(3 < 2 || (0 < 1 && 3 >= 3)) && true
+//
+//!!(!!true && !!false)
+//
+//true && (true && (true && (true || false)))
+
+
+// 3.
+/*
+ You are given two variables a and b, compute their sum, store it in a variable named sum, then print the result.
+
+ Example 1
+ Input:
+ var a = 1
+ var b = 2
+
+ Expected values:
+ sum = 3
+
+ Output:
+ 3
+
+ Example 2
+ Input:
+ var a = 13
+ var b = 22
+
+ Expected values:
+ sum = 35
+
+ Output:
+ 35
+*/
+
+
+// 4.
+
+// Determine the number of seconds in a year and store the number in a variable named secondsInAYear.
+
+/*
+ Hint:
+ The number of seconds in a year is 365 times the number of seconds in a day.
+ The number of seconds in a day is 24 times the number of seconds in a hour.
+ The number of seconds in a hour is 60 times the number of seconds in a minute, which is 60.
+*/
+
+
+// 5
+/*
+ Your are given the width and height of a screen in pixels.
+ Calculate the total number of pixels on the screen and store the result in a variable named numberOfPixels.
+*/
+
+/*
+ var width = 1920
+ var height = 1080
+
+ Example 1
+ Input:
+ var width = 4
+ var height = 3
+
+ Expected values:
+ numberOfPixels = 12
+
+ Example 2
+ Input:
+ var width = 1920
+ var height = 1080
+
+ Expected values:
+ numberOfPixels = 2073600
+
+ Hint:
+ Consider a 5x3 screen like this:
+ *****
+ *****
+ *****
+
+ The number of pixels on this screen is 5+5+5 = 5*3
+*/
+
+
+// 6
+
+/*
+
+ You are given the sum and the difference of two numbers.
+ Find out the values of the original numbers and store them in variables a and b.
+*/
+
+
+// 7.
+
+/*
+ Given two variable a and b, swap their values.
+ That is the new value of a will become the old value of b and vice versa.
+*/
+
+/*
+ var a = 1
+ var b = 2
+
+ Example 1
+ Input:
+ a = 2
+ b = 1
+
+ Hint:
+ Just assigning a to the value of b and b to the value of a will not work.
+
+ var a = 1
+ var b = 2
+
+ a = b // a will have the value 2. The original value of a is lost
+ b = a // b will remain the same
+*/
+
+
+// 8.
+// You are given a number a. Print the last digit of a.
+
+/*
+ var a = 123
+
+ Example 1
+ Input:
+ var a = 123
+
+ Output:
+ 3
+
+ Example 2
+ Input:
+ var a = 337
+
+ Output:
+ 7
+
+ Hint:
+ Use the remainder % operator.
+*/
+
+
+// 9.
+/*
+ You are given Rocky’s age in dog years. Print Rocky’s age in human years.
+ You know that 1 human year is 7 dog years.
+*/
+
+/*
+ Example 1
+ Input:
+ var rockysAgeInDogYears = 50
+
+ Output:
+ 7
+*/
+
+
+// 10.
+
+/*
+ x years from now Alice will be y times older than her brother Bob. Bob is 12 years old. How old is Alice?
+*/
+
+/*
+ var x = 3
+ var y = 2
+ var bob = 12
+
+ var alice = ?
+
+ Example 1
+ Input:
+ var x = 3
+ var y = 2
+ var bob = 12
+
+ Expected values:
+ alice = 27
+
+ Example 2
+ Input:
+ var x = 1
+ var y = 3
+ var bob = 12
+
+ Expected values:
+ alice = 38
+
+ Hint:
+ alice + x = y * (bob + x)
+ Solve for alice
+*/
+
+// 11. 
+/*
+You have x apples. Bob trades 3 oranges for 5 apples. He does not accept trades with cut fruit. How many oranges can you get from Bob and how many apples will you have left? The number of apples you will have left should be stored in a variable named apples. The number of oranges you will have after the trade should be stored in a variable named oranges.
+*/
+
+/*
+var x = 17
+
+Example 1
+Input: 
+var x = 17
+
+Expected values: 
+apples = 2
+oranges = 9
+
+Example 2
+Input: 
+var x = 25
+
+Expected values: 
+apples = 0
+oranges = 15
+
+Hint:
+Use the division(/) and the remainder(%) operator
+*/
+
+// 12. 
+/*
+A class consists of numberOfBoys boys and numberOfGirls girls.
+
+Print the percentage of boys in the class followed by the percentage of girls in the class. The percentage should be printed rounded down to the nearest integer. For example 33.333333333333 will be printed as 33.
+*/
+
+/*
+var numberOfBoys = 20
+var numberOfGirls = 60
+
+Example 1
+Input: 
+var numberOfBoys = 20  
+var numberOfGirls = 60
+
+Output:
+25 // percentage of boys
+75 // percentage of girls
+*/
+
+
+// 13. 
+// Which of the following expressions evaluate to true?
+
+/*
+a. 3 < 12.3
+b. 9 == 2
+c. "Hello!" == "Hello!"
+d. 19.0 >= 19.0
+e. 9 > 7 && 7 < 10
+*/ 
+
